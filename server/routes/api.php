5da26b335756e6182c31ad9786b82ac91cc92622
Yahia-cherrat <78372992+Yahia-cherrat@users.noreply.github.com>
2024-10-24 17:57:15 +0100

    Setup project & create restAPI in laravel

---
+++ server/routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContractArticleController;
+
+// Route to get authenticated user details
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// ContractArticles Routes
+
+Route::get('/contract-articles', [ContractArticleController::class, 'index']);
+Route::post('/contract-articles', [ContractArticleController::class, 'store']);
+Route::get('/contract-articles/{id}', [ContractArticleController::class, 'show']);
+Route::put('/contract-articles/{id}', [ContractArticleController::class, 'update']);
+Route::delete('/contract-articles/{id}', [ContractArticleController::class, 'destroy']);
